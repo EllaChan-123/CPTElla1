5d6b213b386624b2457820a39581a28a60185e9a
EllaChan-123 <ella.chan26@ycdsbk12.ca>
2025-05-23 09:33:13 -0400

    CPT Framework test

--- CPTElla.java
+++ CPTElla.java
@@ -3,6 +3,11 @@ import arc.*;
 public class CPTELla{
 	public static void main(String[] args){
 		Console con = new Console();
+		con.println("Hello world");
+		int intNum;
+		con.println("what is your lucky number");
+		intNum = con.readInt();
+		con.println("lucky number "=intNume+"!");
 		
 	}
 }
