d92659f65920f1919e4e6f0c1dfefdaec8d97c7d
EllaChan-123 <ella.chan26@ycdsbk12.ca>
2025-05-29 09:25:32 -0400

    Update CPTElla.java

--- CPTElla.java
+++ CPTElla.java
@@ -1,13 +1,12 @@
 import arc.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
+import java.awt.Font;
 
 public class CPTElla{
 	public static void main(String[] args){
-		Console con = new Console();
-		con.println("Hello world");
-		int intNum;
-		con.println("what is your lucky number");
-		intNum = con.readInt();
-		con.println("lucky number "+intNum+"!");
+		Console con = new Console("Multiple Choice");
+		
 		
 	}
 }
