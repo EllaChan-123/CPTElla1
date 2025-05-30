a3c0fcf179ecb574b0a7bea8764cf8977611b867
EllaChan-123 <ella.chan26@ycdsbk12.ca>
2025-05-30 09:31:26 -0400

    Added comments to the code
    Described the purpose of each section of the code

--- CPTElla.java
+++ CPTElla.java
@@ -9,15 +9,21 @@ public class CPTElla{
 		boolean blnMain = true;
 		int intChoice;
 		
-		MainMenu(blnMain, con);
-		
-		
-		
+		//Display the main menu as soon as the game starts
+		if (blnMain == true){
+			MainMenu(blnMain, con);
+		}
+			
 	}
 	public static void MainMenu(boolean blnMain, Console con){
+		//Create the appearence of the main menu
+		
 		while(blnMain == true){
+			//Set background
 			con.setDrawColor(new Color(86, 116, 142));
 			con.fillRect(0, 0, 1100, 700);
+			//Load name of the game
+			
 		}
 		
 	}
