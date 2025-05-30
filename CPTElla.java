3ee71a5533ba8ad5db5d6fb4cc83e0bd019bc941
EllaChan-123 <ella.chan26@ycdsbk12.ca>
2025-05-30 09:02:08 -0400

    Created method for the main menu
    Also drew the background for main menu and created a boolean trigger for it.

--- CPTElla.java
+++ CPTElla.java
@@ -5,8 +5,20 @@ import java.awt.Font;
 
 public class CPTElla{
 	public static void main(String[] args){
-		Console con = new Console("Multiple Choice");
+		Console con = new Console("Multiple Choice", 1100, 700);
+		boolean blnMain = true;
+		int intChoice;
 		
+		MainMenu(blnMain, con);
+		
+		
+		
+	}
+	public static void MainMenu(boolean blnMain, Console con){
+		while(blnMain == true){
+			con.setDrawColor(new Color(86, 116, 142));
+			con.fillRect(0, 0, 1100, 700);
+		}
 		
 	}
 }
